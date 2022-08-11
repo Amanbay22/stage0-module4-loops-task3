d3eaeec815e79dc1f4a7f668473e5a301010f85e
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-11 17:04:37 +0600

    easy task

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
 
+        int x = 0;
+        int sum = 0;
+        for (int i = 0; i < lengthOfLastNumber; i++) {
+            x = x + (int) Math.pow(10, i) * 9;
+            sum +=x;
+        }
+
+        System.out.println(sum);
     }
 }
